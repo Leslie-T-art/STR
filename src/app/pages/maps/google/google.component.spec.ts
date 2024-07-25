aae388089cdcb6a14e3d91a8c76ce22fc1432c98
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-25 11:24:46 +0200

    Create google.component.spec.ts

---
+++ src/app/pages/maps/google/google.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GoogleComponent } from './google.component';
+
+describe('GoogleComponent', () => {
+  let component: GoogleComponent;
+  let fixture: ComponentFixture<GoogleComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [GoogleComponent]
+    });
+    fixture = TestBed.createComponent(GoogleComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
