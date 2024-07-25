2d40a748bb306273be827dfedef49ea45802aa74
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-25 12:03:53 +0200

    Create privacy-policy.component.ts

---
+++ src/app/pages/extrapages/privacy-policy/privacy-policy.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-privacy-policy',
+  templateUrl: './privacy-policy.component.html',
+  styleUrls: ['./privacy-policy.component.scss']
+})
+
+// Privacy Policy Component
+export class PrivacyPolicyComponent {
+
+  // bread crumb items
+  breadCrumbItems!: Array<{}>;
+
+  ngOnInit(): void {
+
+    /**
+   * BreadCrumb
+   */
+    this.breadCrumbItems = [
+      { label: 'Pages', active: true },
+      { label: 'Privacy Policy', active: true }
+    ];
+  }
+
+}
