338ab82b42a6acf843e5e0b48b63edebcd69da0f
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-25 11:58:37 +0200

    Create bootstrap.component.spec.ts

---
+++ src/app/pages/icons/bootstrap/bootstrap.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BootstrapComponent } from './bootstrap.component';
+
+describe('BootstrapComponent', () => {
+  let component: BootstrapComponent;
+  let fixture: ComponentFixture<BootstrapComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [BootstrapComponent]
+    });
+    fixture = TestBed.createComponent(BootstrapComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
