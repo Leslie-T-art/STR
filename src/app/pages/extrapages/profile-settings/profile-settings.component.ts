2a180e1f368ec3694516341cb633d71079ea222a
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-25 12:04:00 +0200

    Create profile-settings.component.ts

---
+++ src/app/pages/extrapages/profile-settings/profile-settings.component.ts
@@ -0,0 +1,103 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-profile-settings',
+  templateUrl: './profile-settings.component.html',
+  styleUrls: ['./profile-settings.component.scss']
+})
+
+// Profile Setting Component
+export class ProfileSettingsComponent {
+
+
+  // bread crumb items
+  breadCrumbItems!: Array<{}>;
+  fieldTextType!: boolean;
+  fieldTextType1!: boolean;
+  fieldTextType2!: boolean;
+
+  formGroups: FormGroup[] = [];
+  educationForm!: FormGroup;
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+
+    /**
+   * BreadCrumb
+   */
+    this.breadCrumbItems = [
+      { label: 'Pages', active: true },
+      { label: 'Profile Settings', active: true }
+    ];
+
+    this.educationForm = this.formBuilder.group({
+      degree: [''],
+      name: [''],
+      year: [''],
+      to: [''],
+      description: ['']
+    });
+    this.formGroups.push(this.educationForm);
+  }
+
+  // File Upload
+  imageURL: any;
+  fileChange(event: any, id: any) {
+    let fileList: any = (event.target as HTMLInputElement);
+    let file: File = fileList.files[0];
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imageURL = reader.result as string;
+      if (id == '0') {
+        document.querySelectorAll('#cover-img').forEach((element: any) => {
+          element.src = this.imageURL;
+        });
+      }
+      if (id == '1') {
+        document.querySelectorAll('#user-img').forEach((element: any) => {
+          element.src = this.imageURL;
+        });
+      }
+    }
+
+    reader.readAsDataURL(file)
+  }
+
+  /**
+   * skills Select2
+   */
+  Skills = [
+    { name: 'Illustrator' },
+    { name: 'Photoshop' },
+    { name: 'CSS' },
+    { name: 'HTML' },
+    { name: 'Javascript' },
+    { name: 'Python' },
+    { name: 'PHP' },
+  ];
+
+  /**
+   * Password Hide/Show
+   */
+  toggleFieldTextType() {
+    this.fieldTextType = !this.fieldTextType;
+  }
+  toggleFieldTextType1() {
+    this.fieldTextType1 = !this.fieldTextType1
+  }
+  toggleFieldTextType2() {
+    this.fieldTextType2 = !this.fieldTextType2;
+  }
+
+  // add Form
+  addForm() {
+    const formGroupClone = this.formBuilder.group(this.educationForm.value);
+    this.formGroups.push(formGroupClone);
+  }
+
+  // Delete Form
+  deleteForm(id: any) {
+    this.formGroups.splice(id, 1)
+  }
+}
