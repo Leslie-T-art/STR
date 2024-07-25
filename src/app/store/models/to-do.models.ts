88a4b38f8081e79ec0393713f99dec3d73b0adc0
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-25 11:20:59 +0200

    Create to-do.models.ts

---
+++ src/app/store/models/to-do.models.ts
@@ -0,0 +1,11 @@
+
+// Contact Modal
+export interface TodoModel {
+    id?: any;
+    task?: any;
+    subItem?: any;
+    dueDate?: any;
+    status?: any;
+    priority?: any;
+    checked?: any;
+  }
