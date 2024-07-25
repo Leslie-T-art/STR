6d39f32373ee2ef0d67b771285d5d07369d0ffdd
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-25 11:25:42 +0200

    Create pages-routing.module.ts

---
+++ src/app/pages/pages-routing.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '', loadChildren: () => import('./dashboards/dashboards.module').then(m => m.DashboardsModule)
+  },
+  {
+    path: 'apps', loadChildren: () => import('./apps/apps.module').then(m => m.AppsModule)
+  },
+  {
+    path: 'ui', loadChildren: () => import('./ui/ui.module').then(m => m.UiModule)
+  },
+  {
+    path: 'invoices', loadChildren: () => import('./invoices/invoices.module').then(m => m.InvoicesModule)
+  },
+  {
+    path: 'advance-ui', loadChildren: () => import('./advance-ui/advance-ui.module').then(m => m.AdvanceUiModule)
+  },
+  {
+    path: 'maps', loadChildren: () => import('./maps/maps.module').then(m => m.MapsModule)
+  },
+  {
+    path: 'icons', loadChildren: () => import('./icons/icons.module').then(m => m.IconsModule)
+  },
+  {
+    path: 'charts', loadChildren: () => import('./charts/charts.module').then(m => m.ChartsModule)
+  },
+  {
+    path: 'tables', loadChildren: () => import('./tables/tables.module').then(m => m.TablesModule)
+  },
+  {
+    path: 'forms', loadChildren: () => import('./forms/forms.module').then(m => m.FormModule)
+  },
+  {
+    path: 'pages', loadChildren: () => import('./extrapages/extrapages.module').then(m => m.ExtrapagesModule)
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PagesRoutingModule { }
