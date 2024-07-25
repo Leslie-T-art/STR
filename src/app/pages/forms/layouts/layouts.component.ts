ba5a361c2f3f062df3ac0b925420b514156bd029
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-25 11:57:32 +0200

    Create layouts.component.ts

---
+++ src/app/pages/forms/layouts/layouts.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layouts',
+  templateUrl: './layouts.component.html',
+  styleUrls: ['./layouts.component.scss']
+})
+export class LayoutsComponent {
+
+  colorTheme: any = 'theme-blue';
+
+  // bread crum items
+  breadCrumbItems!: Array<{}>;
+
+  ngOnInit(): void {
+    this.breadCrumbItems = [{ label: 'Forms' }, { label: 'Form Layout', active: true }];
+  }
+
+}
