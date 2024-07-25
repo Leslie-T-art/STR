b5fa159b290c87e8960858d39b39dde011fc9df3
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-25 11:58:50 +0200

    Create phosphor.component.ts

---
+++ src/app/pages/icons/phosphor/phosphor.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+// Data Get
+import { phosphoricons } from '../../../core/data/phosphor';
+
+@Component({
+  selector: 'app-phosphor',
+  templateUrl: './phosphor.component.html',
+  styleUrls: ['./phosphor.component.scss']
+})
+export class PhosphorComponent {
+  // bread crumb items
+  breadCrumbItems!: Array<{}>;
+  icons!: any;
+  ngOnInit(): void {
+    /**
+   * BreadCrumb
+   */
+    this.breadCrumbItems = [
+      { label: 'Icons' },
+      { label: 'Phosphor', active: true }
+    ];
+
+    setTimeout(() => {
+      var keys = Object.keys(phosphoricons);
+      this.loadIconList(keys)
+    }, 100);
+
+  }
+
+  loadIconList(datas: any) {
+    var icons = '';
+    var arr = Array.from(datas);
+    for (let index = 0; index < arr.length; index++) {
+      icons += '<div class="col-xl-3 col-lg-4 col-sm-6">\
+     <i class="'+ arr[index] + '"></i> ' + arr[index] + '\
+     </div>'
+    }
+    const element: HTMLElement = document.getElementById('iconList') as HTMLElement
+    element.innerHTML = icons
+  };
+}
