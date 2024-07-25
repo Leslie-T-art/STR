df2487f3068e3280ab92b0e0082a0951ffe0a828
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-25 11:32:31 +0200

    Create alerts.component.spec.ts

---
+++ src/app/pages/ui/alerts/alerts.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AlertsComponent } from './alerts.component';
+
+describe('AlertsComponent', () => {
+  let component: AlertsComponent;
+  let fixture: ComponentFixture<AlertsComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AlertsComponent]
+    });
+    fixture = TestBed.createComponent(AlertsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
