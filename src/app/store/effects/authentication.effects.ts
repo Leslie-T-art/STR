acb5197f75b4883113facf1bfaa551dcf0ccac02
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-25 11:37:09 +0200

    Create authentication.effects.ts

---
+++ src/app/store/effects/authentication.effects.ts
@@ -0,0 +1,88 @@
+import { Injectable, Inject } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { map, switchMap, catchError, exhaustMap, tap } from 'rxjs/operators';
+import { from, of } from 'rxjs';
+import { AuthenticationService } from '../../core/services/auth.service';
+import { login, loginSuccess, loginFailure, logout, logoutSuccess, Register, signInWithFacebook, signInWithGoogle } from '../actions/authentication.actions';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class AuthenticationEffects {
+
+  Register$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(Register),
+      exhaustMap(({ email, first_name, password }) =>
+        this.AuthenticationService.register(email, first_name, password).pipe(
+          map((user) => {
+            this.router.navigate(['/auth/login']);
+            return loginSuccess({ user });
+          }),
+          catchError((error) => of(loginFailure({ error })))
+        )
+      )
+    )
+  );
+
+  login$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(login),
+      exhaustMap(({ email, password }) =>
+        this.AuthenticationService.login(email, password).pipe(
+          map((user) => {
+            localStorage.setItem('currentUser', JSON.stringify(user.data));
+            localStorage.setItem('token', user.token);
+            this.router.navigate(['/']);
+            return loginSuccess({ user });
+          }),
+          catchError((error) => of(loginFailure({ error })))
+        )
+      )
+    )
+  );
+
+  signInWithFacebook$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(signInWithFacebook),
+      exhaustMap(() =>
+        from(this.AuthenticationService.signInWithFacebook()).pipe(
+          map(user => {
+            return loginSuccess({ user });
+          }),
+          catchError(error => of(loginFailure({ error })))
+        )
+      )
+    )
+  );
+
+  signInWithGoogle$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(signInWithGoogle),
+      exhaustMap(() =>
+        from(this.AuthenticationService.signInWithGoogle()).pipe(
+          map(user => {
+            return loginSuccess({ user });
+          }),
+          catchError(error => of(loginFailure({ error })))
+        )
+      )
+    )
+  );
+
+
+  logout$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(logout),
+      tap(() => {
+        // Perform any necessary cleanup or side effects before logging out
+      }),
+      exhaustMap(() => of(logoutSuccess()))
+    )
+  );
+
+  constructor(
+    @Inject(Actions) private actions$: Actions,
+    private AuthenticationService: AuthenticationService,
+    private router: Router) { }
+
+}
