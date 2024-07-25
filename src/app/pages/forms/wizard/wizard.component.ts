b42c22db5b1204a63f7c7d96a470c4b7a8cd31c3
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-25 11:58:23 +0200

    Create wizard.component.ts

---
+++ src/app/pages/forms/wizard/wizard.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wizard',
+  templateUrl: './wizard.component.html',
+  styleUrls: ['./wizard.component.scss']
+})
+export class WizardComponent {
+  // bread crumb items
+  breadCrumbItems!: Array<{}>;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    /**
+    * BreadCrumb
+    */
+    this.breadCrumbItems = [
+      { label: 'Forms' },
+      { label: 'Wizard', active: true }
+    ];
+  }
+
+}
