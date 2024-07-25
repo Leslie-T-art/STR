158a9bb6f756fd41d57fb9668aaf193d64ad8000
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-25 12:03:37 +0200

    Create faqs.component.ts

---
+++ src/app/pages/extrapages/faqs/faqs.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-faqs',
+  templateUrl: './faqs.component.html',
+  styleUrls: ['./faqs.component.scss']
+})
+
+// Faq Component
+export class FaqsComponent {
+
+  // bread crumb items
+  breadCrumbItems!: Array<{}>;
+  isFirstOpen = true
+
+  ngOnInit(): void {
+
+    /**
+   * BreadCrumb
+   */
+    this.breadCrumbItems = [
+      { label: 'Pages', active: true },
+      { label: 'FAQs', active: true }
+    ];
+  }
+}
