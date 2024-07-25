52663c0a29a7eb6b82d59f322f8aa940a24c252c
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-25 11:33:14 +0200

    Create colors.component.ts

---
+++ src/app/pages/ui/colors/colors.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-colors',
+  templateUrl: './colors.component.html',
+  styleUrls: ['./colors.component.scss']
+})
+export class ColorsComponent {
+  // bread crum items
+  breadCrumbItems!: Array<{}>;
+
+  ngOnInit(): void {
+    this.breadCrumbItems = [{ label: 'Base UI' }, { label: 'Colors', active: true }];
+  }
+}
